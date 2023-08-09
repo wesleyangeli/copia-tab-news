1bf3748acb96af3856fa3d67f2e02486b656ff22
Wesley Angeli <58198030+wesleyangeli@users.noreply.github.com>
2023-08-09 02:11:14 +0000

    feat: primeira pagina dedicatoria

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+function Home () {
+    return (
+        <div style={{background:"#222", whidth: "100%", height: "100vh", margin: "0", padding: "50px", color: "white", fontFamily: "sans-serif", fontSize: "20px"} }>
+            <p>
+                Meu amor, te admiro a cada dia que passa, você é muito batalhadora, uma mãe maravilhosa e dedicada.
+            </p>
+            <p>
+                Essa é a minha primeira página criada sozinho e dedico ela pra você, porque sempre esteve ao meu lado, me apoiando em todo momento.
+            </p>
+            <p>
+                Obrigado por tudo meu amor, sem você eu não seria completo. Te amo! 🥰
+            </p>
+        </div>
+    )
+}
+
+export default Home
