d3ad7e2ae306e96d25718223db218ab18ec2f2c1
Wesley Angeli <58198030+wesleyangeli@users.noreply.github.com>
2024-05-24 01:14:39 +0000

    install jest e configura scripts de testes

--- pages/index.js
+++ pages/index.js
@@ -6,22 +6,13 @@ function Home() {
         whidth: "100%",
         height: "100vh",
         margin: "0",
-        padding: "50px",
+        padding: "0",
         color: "white",
         fontFamily: "sans-serif",
         fontSize: "20px",
       }}
     >
-      <p>
-        Meu amor, te admiro a cada dia que passa, você é muito batalhadora, uma
-        mãe maravilhosa e dedicada.
-      </p>
-      <p>
-        Essa é a minha primeira página criada sozinho e dedico ela pra você,
-        porque sempre esteve ao meu lado, me apoiando em todo momento.
-      </p>
-      <p>Obrigado por tudo meu amor, sem você eu não seria completo.</p>
-      <p>Te amo! 🥰</p>
+      <p>EM CONSTRUÇÃO</p>
     </div>
   );
 }
