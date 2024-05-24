1ae9e3a3ed56e7246b94c3d1caa0c2d0303badbc
Wesley Angeli <58198030+wesleyangeli@users.noreply.github.com>
2024-05-24 18:45:26 +0000

    add arquivos "database.js" e "env"

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,3 +1,7 @@
-export default function status(request, response) {
+import database from "../../../../infra/database.js";
+
+export default async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
+  console.log(result.rows);
   response.status(200).json({ chave: "são a cima da média" });
 }
