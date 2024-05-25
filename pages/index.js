0d9443ffe44df94b02dec2f745c89588636d75d4
Wesley Angeli <58198030+wesleyangeli@users.noreply.github.com>
2024-05-25 15:55:25 +0000

    style background page index.js

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,11 @@ function Home() {
   return (
     <div
       style={{
-        background: "#222",
         whidth: "100%",
         height: "100vh",
         margin: "0",
         padding: "0",
-        color: "white",
+        color: "#000",
         fontFamily: "sans-serif",
         fontSize: "20px",
       }}
